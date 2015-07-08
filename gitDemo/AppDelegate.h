97ce4014a2ed1f9b5202be55e483be6aa16e961f
Tree <tree@TreedeMacBook-Pro.local>
2015-07-08 16:22:13 +0800

    不知道

---
+++ gitDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  gitDemo
+//
+//  Created by Tree on 15/7/8.
+//  Copyright (c) 2015年 wayoukeji. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ gitDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  gitDemo
+//
+//  Created by Tree on 15/7/8.
+//  Copyright (c) 2015年 wayoukeji. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
